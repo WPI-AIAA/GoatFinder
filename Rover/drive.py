a5a5c3aee6837b4a21f8f1f437db0e3cb7e1f042
NathanSiegel <siegelnathan08@gmail.com>
2018-02-11 18:54:29 -0500

    made it so that the drive function actually has a loop that can exit

--- Rover/drive.py
+++ Rover/drive.py
@@ -8,39 +8,43 @@ def wheeltravel():
     #placeholder. determines distance the rover has traveled since the last update
     return [0.0,0.0]
 
+def iscloseenough(tgtlocation, location, margin):
+	dispx = tgtlocation[0] - location[0]
+	dispy = tgtlocation[1] - location[1]
+	if dispx < margin and -dispx < margin and dispy < margin and -dispy < margin:
+		return True
+	return False
+	
 def drive(tgtlocation, location):
-    #tgtlocation and location are arrays with x and y-distances
-    dircontroller = PID(1,1,1,.5)
-    speedcontroller = PID(1,0,1,.5)
-    
-    direction = compass()
-    delta = wheeltravel()
-    delta = [delta[1]*math.sin(direction)-delta[0]*math.cos(direction),delta[1]*math.cos(direction)-delta[0]*math.sin(direction)]
+	while iscloseenough(tgtlocation, location, 1.0): #we should determine what the correct 
+		#tgtlocation and location are arrays with x and y-distances
+		dircontroller = PID(1,1,1,.5)
+		speedcontroller = PID(1,0,1,.5)
+		
+		direction = compass()
+		delta = wheeltravel()
+		delta = [delta[1]*math.sin(direction)-delta[0]*math.cos(direction),delta[1]*math.cos(direction)-delta[0]*math.sin(direction)]
 
-    speed = math.sqrt(math.pow(delta[0],2)+math.pow(delta[1],2))
+		speed = math.sqrt(math.pow(delta[0],2)+math.pow(delta[1],2))
 
-    location[0] += delta[0]
-    location[1] += delta[1]
+		location[0] += delta[0]
+		location[1] += delta[1]
 
-    distanceleft = [tgtlocation[0]-location[0],tgtlocation[1]-location[1]]
+		distanceleft = [tgtlocation[0]-location[0],tgtlocation[1]-location[1]]
 
-    direction = math.atan2(distanceleft[1], distanceleft[0])
-    distance = math.sqrt(math.pow(distanceleft[0],2)+math.pow(distanceleft[1],2))
+		direction = math.atan2(distanceleft[1], distanceleft[0])
+		distance = math.sqrt(math.pow(distanceleft[0],2)+math.pow(distanceleft[1],2))
 
-    tgtspeed = speedcontroller.update(distance/speed) #optimal speed for rover to travel at
-    tgtdirection = dircontroller.update(direction) #amount for rover to turn CW
+		tgtspeed = speedcontroller.update(distance/speed) #optimal speed for rover to travel at
+		tgtdirection = dircontroller.update(direction) #amount for rover to turn CW
 
-    if tgtspeed > 1:
-        tgtspeed = 1
+		if tgtspeed > 1:
+			tgtspeed = 1
 
-    motorspeed = [tgtspeed+tgtdirection,tgtspeed-tgtdirection]
-    if motorspeed[0] > motorspeed[1]:
-        motorspeed = [1,motorspeed[1]/motorspeed[0]]
-    else:
-        motorspeed = [motorspeed[0]/motorspeed[1],1]
-    motorspeed = [motorspeed[0]*tgtspeed, motorspeed[1]*tgtspeed]
-    #TODO: write values to motor
-
-    
-
-    
+		motorspeed = [tgtspeed+tgtdirection,tgtspeed-tgtdirection]
+		if motorspeed[0] > motorspeed[1]:
+			motorspeed = [1,motorspeed[1]/motorspeed[0]]
+		else:
+			motorspeed = [motorspeed[0]/motorspeed[1],1]
+		motorspeed = [motorspeed[0]*tgtspeed, motorspeed[1]*tgtspeed]
+		#TODO: write values to motor
