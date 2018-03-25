bbce541cb34f4e685811221ae3fb428921e91b3b
NathanSiegel <siegelnathan08@gmail.com>
2018-03-25 18:55:03 -0400

    started IO macro bit

---
+++ Rover/inputOutputMacro.py
@@ -0,0 +1,21 @@
+#Predefined globals for input/output
+
+#replace strings with actual locations
+
+'''
+__genericAsyncSerial__ = ("TX","RX")
+__genericI2C__ = ("SDA","SCL")
+__genericSPI__ = ("MISO","MOSI","SCK")
+__genericSPIDevice__ = ("CS")
+__genericServo__ = ("PWM")
+__genericMotor__ = ("A","B","PWM")
+'''
+
+usingPCB = True
+
+if usingPCB:
+    __leftMotor__ = ("A","B","PWM")
+    __rightMotor__ = ("A","B","PWM")
+    __marker1Servo__ = ("PWM")
+    __marker1Servo__ = ("PWM")
+    __parachuteServo__ = ("PWM")
