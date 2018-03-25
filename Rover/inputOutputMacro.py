f9ff87d84a2872609ff0bc3e138c94cbec4349fe
NathanSiegel <siegelnathan08@gmail.com>
2018-03-25 19:00:41 -0400

    who knows which pins we're using? not this guy

--- Rover/inputOutputMacro.py
+++ Rover/inputOutputMacro.py
@@ -11,9 +11,19 @@ __genericServo__ = ("PWM")
 __genericMotor__ = ("A","B","PWM")
 '''
 
+import RPi.GPIO as GPIO
+
 usingPCB = True
 
 if usingPCB:
+    __leftMotor__ = ("A","B","PWM")
+    __rightMotor__ = ("A","B","PWM")
+    __marker1Servo__ = ("PWM")
+    __marker1Servo__ = ("PWM")
+    __parachuteServo__ = ("PWM")
+
+    #do other things
+else:
     __leftMotor__ = ("A","B","PWM")
     __rightMotor__ = ("A","B","PWM")
     __marker1Servo__ = ("PWM")
