247cf7978a40716ab08edd3ac250357d0294caa6
NathanSiegel <siegelnathan08@gmail.com>
2018-04-01 18:40:32 -0400

    added old function queue stuff

--- Rover/main.py
+++ Rover/main.py
@@ -12,3 +12,25 @@ class State(Enum):
 
 
 # Go Into Operation Mode
+
+
+#Nathan's really old stuff (w/ two example functions)
+from PIDcontrol import PID
+from functionQueue import *
+
+def doAThing(arg1, arg2):
+    print(arg1)
+    print(arg2)
+def doAnotherThing(arg1):
+    print(arg1)
+
+if __name__ == '__main__':
+    #testing PID
+    #controller = PID(1,1,1,.5)
+
+    #you can add functions to the queue
+    addToQueue((doAThing, "ASDASDJSAFKJHASFKHE", "asdfs"))
+    addToQueue((doAnotherThing, 3+4))
+
+    #you can run the queue, it'll run
+    mainQueue()
