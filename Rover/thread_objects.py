9de958b0d444aeb2105dc416ff8f9b66bc8d9c42
Zane Weissman <zweissman@wpi.edu>
2018-04-04 17:56:07 -0400

    started on threads

--- Rover/thread_objects.py
+++ Rover/thread_objects.py
@@ -1,5 +1,8 @@
-class hall_thread:
+import LSM9DS0
+class hall_thread:j
     pass
 
 class ninedof_thread:
+    global nav
+    nav.new_9dof((LSM9DS0.readGyro(),LSM9DS0.readMag(),LSM9DS0.readAccel()))
     pass
