a0a2ec1e96a89a4ddee309d1c80e1c9e77efeda2
NathanSiegel <siegelnathan08@gmail.com>
2018-04-08 11:56:17 -0400

    servo PWM controller should be done, including motors, droppers, and parachute

--- Rover/motorOutput.py
+++ Rover/motorOutput.py
@@ -1,26 +1,29 @@
 #sue me
 
 import inputOutputMacro as iom
+import pwmHelper as pwmH
 import RPI.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(iom.__leftMotor__[0], GPIO.OUT)
 GPIO.setup(iom.__leftMotor__[1], GPIO.OUT)
-leftmotorPWM = GPIO.PWM(iom.__leftMotor__[2], 100)
-leftmotorPWM.start(0)
+#leftmotorPWM = GPIO.PWM(iom.__leftMotor__[2], 100)
+#leftmotorPWM.start(0)
 
 GPIO.setup(iom.__rightMotor__[0], GPIO.OUT)
 GPIO.setup(iom.__rightMotor__[1], GPIO.OUT)
-rightmotorPWM = GPIO.PWM(iom.__rightMotor__[2], 100)
-rightmotorPWM.start(0)
+#rightmotorPWM = GPIO.PWM(iom.__rightMotor__[2], 100)
+#rightmotorPWM.start(0)
 
 def startmotors():
-    leftmotorPWM.start(0)
-    rightmotorPWM.start(0)
+    #leftmotorPWM.start(0)
+    #rightmotorPWM.start(0)
 
 def stopmotors():
-    leftmotorPWM.stop()
-    rightmotorPWM.stop()
+    #leftmotorPWM.stop()
+    #rightmotorPWM.stop()
+    pwmH.set_leftmotor(0)
+    pwmH.set_rightmotor(0)
     GPIO.output(iom.__leftMotor__[0], False)
     GPIO.output(iom.__leftMotor__[1], False)
     GPIO.output(iom.__rightMotor__[0], False)
@@ -35,11 +38,14 @@ def driveleftmotor(velocity):
     if velocity >= 0:
         GPIO.output(iom.__leftMotor__[0], True)
         GPIO.output(iom.__leftMotor__[1], False)
-        leftmotorPWM.ChangeDutyCycle(100*velocity)
+        #leftmotorPWM.ChangeDutyCycle(100*velocity)
+        pwmH.set_leftmotor(100*velocity)
+        
     else:
         GPIO.output(iom.__leftMotor__[0], False)
         GPIO.output(iom.__leftMotor__[1], True)
-        leftmotorPWM.ChangeDutyCycle(-100*velocity)
+        #leftmotorPWM.ChangeDutyCycle(-100*velocity)
+        pwmH.set_leftmotor(-100*velocity)
         
 def driverightmotor(velocity):
     if velocity > 1.0:
@@ -50,9 +56,11 @@ def driverightmotor(velocity):
     if velocity >= 0:
         GPIO.output(iom.__rightMotor__[0], True)
         GPIO.output(iom.__rightMotor__[1], False)
-        rightmotorPWM.ChangeDutyCycle(100*velocity)
+        #rightmotorPWM.ChangeDutyCycle(100*velocity)
+        pwmH.set_rightmotor(100*velocity)
     else:
         GPIO.output(iom.__rightMotor__[0], False)
         GPIO.output(iom.__rightMotor__[1], True)
-        rightmotorPWM.ChangeDutyCycle(-100*velocity)
+        #rightmotorPWM.ChangeDutyCycle(-100*velocity)
+        pwmH.set_rightmotor(-100*velocity)
         
