2b8123c93016753aefc41c2419ce2ec97099ec2d
pi <zweissman@wpi.edu>
2018-04-10 02:34:49 +0000

    redo nav global as separate file

---
+++ Rover/nav_loader.py
@@ -0,0 +1,12 @@
+from navSystem_hall import navsystem
+
+nav = navsystem(
+        xy_frames_stored = 5,
+        sensor_frames_stored = 20,
+        encoder_frames_stored = 20,
+        zero_angle = 0, # TODO modify this to run parallel to rows of field
+        encoder_distance = 2.7474,
+        skid_err_range = .5, # TODO adjust based on tests
+        accel_time_step = .05,# TODO set before running
+        gyro_pitch_axis = 1, # Y/1 for PCB
+        )
--- Rover/thread_objects.py
+++ Rover/thread_objects.py
@@ -1,11 +1,13 @@
 import LSM9DS0
 import time
+from nav_loader import nav
+
 class hall_thread:
     pass
 
-class ninedof_thread(t=0.05):
-    global nav
-    def __init__():
+class nine_dof_thread():
+    #global nav
+    def __init__(t=0.05):
         while True:
             nav.new_9dof((LSM9DS0.readGyro(),LSM9DS0.readMag(),LSM9DS0.readAccel()))
             time.sleep(t)
